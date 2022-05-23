9e649da4a2d9760fd9263379c43083a3ff5b7771
obynodavid12 <obynodavid12@gmail.com>
2022-05-23 12:42:40 -0400

    nnn

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "PERSONAL_ACCESS_TOKEN" {
   description = "GitHub personal access token"
   type        = string
-  default     =""
+  default     = ""
 }
 variable "REPO_OWNER" {
   default = "obynodavid12"
