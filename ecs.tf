6e3e2c68096f1f604a73e512f982f24aee8997f1
obynodavid12 <obynodavid12@gmail.com>
2022-05-22 22:29:30 -0400

    kkkk

--- ecs.tf
+++ ecs.tf
@@ -73,9 +73,9 @@ resource "aws_ecs_task_definition" "task_definition" {
 [
    {
       "name": "ecs-runner",
-      "image": "${fargateapp_image}",
-      "cpu": "${fargate_cpu}",
-      "memory": "${fargate_memory}",
+      "image": "106878672844.dkr.ecr.us-east-2.amazonaws.com/ecs-runner:latest",
+      "cpu": "256",
+      "memory": "512",
       "essential": true,
       "network_mode": "awsvpc",
       "portMappings": [
--- variables.tf
+++ variables.tf
@@ -38,22 +38,3 @@ variable "public_subnet_cidr" {
   default     = "172.31.48.0/20"
 }
 
-variable "fargateapp_image" {
-  description = "Docker image to run in the ECS cluster"
-  default     = "106878672844.dkr.ecr.us-east-2.amazonaws.com/ecs-runner:latest"
-}
-
-variable "fargate_cpu" {
-  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default     = "256"
-}
-
-variable "fargate_memory" {
-  description = "Fargate instance memory to provision (in MiB)"
-  default     = "512"
-}
-
-variable "demoapp_container_name" {
-  description = "Application container name"
-  default     = "demoapp-app"
-}
