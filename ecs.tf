c6dfcc30de55d0e7da4c2eee22c368dab5b0cdfd
obynodavid12 <obynodavid12@gmail.com>
2022-05-22 20:48:00 -0400

    kkkk

--- ecs.tf
+++ ecs.tf
@@ -84,9 +84,59 @@ resource "aws_ecs_task_definition" "task_definition" {
   memory                   = "1024"
   task_role_arn            = aws_iam_role.ecs_task_execution_role.arn
   execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
-  container_definitions    = data.template_file.fargateapp-task-definition-template.rendered
+  container_definitions    = <<TASK_DEFINITION
 
+[
+   {
+      "name": "ecs-runner",
+      "image": "${fargateapp_image}",
+      "cpu": "${fargate_cpu}",
+      "memory": "${fargate_memory}",
+      "essential": true,
+      "network_mode": "awsvpc",
+      "portMappings": [
+        {
+            "containerPort": 80
+        }
+      ],
+      "logConfiguration": {
+        "logDriver": "awslogs",
+        "options": {
+            "awslogs-region" : "us-east-2",
+            "awslogs-group" : "/ecs/${var.prefix}-task-def",
+            "awslogs-stream-prefix" : "ecs"
+        }
+      },
+      "command": ["./start.sh"],
+      "environment": [{
+        "name": "PERSONAL_ACCESS_TOKEN",
+        "value": "${var.PERSONAL_ACCESS_TOKEN}"
+      },
+      {
+        "name": "REPO_OWNER",
+        "value": "${var.REPO_OWNER}"
+      },
+      {
+        "name": "REPO_NAME",
+        "value": "${var.REPO_NAME}"
+      },
+      {
+        "name": "AWS_DEFAULT_REGION",
+        "value": "${var.AWS_DEFAULT_REGION}"
+      },
+      {
+        "name": "AWS_SECRET_ACCESS_KEY",
+        "value": "${var.AWS_SECRET_ACCESS_KEY}"
+      },
+      {
+        "name": "AWS_ACCESS_KEY_ID",
+        "value": "${var.AWS_ACCESS_KEY_ID}"
+      }]
+    }
+]
+  TASK_DEFINITION
 }
+
 # A security group for ECS
 resource "aws_security_group" "ecs_sg" {
   name        = "${var.prefix}-ecs-sg"
